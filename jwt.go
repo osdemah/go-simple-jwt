515f0c6e69d7ddd25a3b8b1f11e93c86cde615ed
Hamed Soleimani <hamed1soleimani@gmail.com>
2016-05-28 20:33:03 +0430

    move jwt generator files here

---
+++ jwt.go
@@ -0,0 +1,104 @@
+package utils
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type JWTPayload struct {
+	Admin  bool
+	Email  string
+	Expire int64
+}
+
+type JWTGenerator struct {
+	Secret []byte
+}
+
+func NewJWTGenerator(secret string) *JWTGenerator {
+	return &JWTGenerator{Secret: []byte(secret)}
+}
+
+func (generator *JWTGenerator) EncodeBase64(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+func (generator *JWTGenerator) DecodeBase64(src string) []byte {
+	dst, _ := base64.StdEncoding.DecodeString(src)
+	return dst
+}
+
+func (generator *JWTGenerator) EncodeJWT(header string, payload string) []byte {
+	return generator.EncodeHMAC(header + "." + payload)
+}
+
+func (generator *JWTGenerator) EncodeHMAC(src string) []byte {
+	mac := hmac.New(sha256.New, generator.Secret)
+	mac.Write([]byte(src))
+	return mac.Sum(nil)
+}
+
+func (generator *JWTGenerator) ValidateJWT(src string) bool {
+	jwt := strings.Split(src, ".")
+	if len(jwt) != 3 {
+		return false
+	}
+	return hmac.Equal(generator.DecodeBase64(jwt[2]),
+		generator.EncodeJWT(jwt[0], jwt[1]))
+}
+
+func (generator *JWTGenerator) GenerateJWT(email string, admin bool) (jwt string) {
+	header := make(map[string]string)
+	payload := make(map[string]string)
+	header["alg"] = "HS256"
+	header["typ"] = "JWT"
+	payload["mail"] = email
+	payload["sub"] = "auth"
+	now := time.Now().Unix()
+	payload["exp"] = strconv.FormatInt(now+5*3600, 10)
+	if admin {
+		payload["admin"] = "1"
+	} else {
+		payload["admin"] = "0"
+	}
+	jsonHeader, _ := json.Marshal(header)
+	jsonPayload, _ := json.Marshal(payload)
+	bs64Header := generator.EncodeBase64(jsonHeader)
+	bs64payload := generator.EncodeBase64(jsonPayload)
+	bs64signature := generator.EncodeBase64(generator.EncodeJWT(bs64Header, bs64payload))
+	jwt = bs64Header + "." + bs64payload + "." + bs64signature
+	return
+}
+
+func (generator *JWTGenerator) RenewJWT(jwt string) (new string) {
+	payload := generator.Decode(jwt)
+	new = generator.GenerateJWT(payload.Email, payload.Admin)
+	return
+}
+
+func (generator *JWTGenerator) CheckExpire(exp int64) (expire bool) {
+	now := time.Now().Unix()
+	expire = now > exp
+	return
+}
+
+func (generator *JWTGenerator) CheckReLogin(exp int64) (relogin bool) {
+	now := time.Now().Unix()
+	relogin = now > exp+3600*24*14
+	return
+}
+
+func (generator *JWTGenerator) Decode(jwt string) (payload JWTPayload) {
+	bs64Payload := generator.DecodeBase64(strings.Split(jwt, ".")[1])
+	mapPayload := make(map[string]string)
+	json.Unmarshal(bs64Payload, &mapPayload)
+	payload.Email = mapPayload["mail"]
+	payload.Admin = mapPayload["admin"] == "1"
+	payload.Expire, _ = strconv.ParseInt(mapPayload["exp"], 10, 64)
+	return
+}
---
+++ jwt_test.go
@@ -0,0 +1,106 @@
+package utils
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestBase64Encoding(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	bs64 := generator.EncodeBase64([]byte("gints means gaming hints!"))
+	expected := "Z2ludHMgbWVhbnMgZ2FtaW5nIGhpbnRzIQ=="
+	if strings.Compare(bs64, expected) != 0 {
+		t.Errorf("expected: %s\n\tgot: %s", expected, bs64)
+	}
+}
+
+func TestBase64Decoding(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	str := string(generator.DecodeBase64("Z2ludHMgbWVhbnMgZ2FtaW5nIGhpbnRzIQ=="))
+	expected := "gints means gaming hints!"
+	if strings.Compare(str, expected) != 0 {
+		t.Errorf("expected: %s\n\tgot: %s", expected, str)
+	}
+}
+
+func TestEncodeHMAC(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	hmac := generator.EncodeBase64(generator.EncodeHMAC("gints means gaming hints!"))
+	expected := "hAooNE4gke+aCxNdvTZgPZCecw6mUrvGQ5pIWg6gDhc="
+	if strings.Compare(hmac, expected) != 0 {
+		t.Errorf("expected: %s\n\tgot: %s", expected, hmac)
+	}
+}
+
+func TestEncodeJWT(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	hmac := generator.EncodeBase64(generator.EncodeJWT("gints", "gaming hints"))
+	expected := "0DY8xD+FPMDOW1cb3PW8ktpUwcgDFPauwxWnMWe42CI="
+	if strings.Compare(hmac, expected) != 0 {
+		t.Errorf("expected: %s\n\tgot: %s", expected, hmac)
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	jwt := "gints.gaming hinst." +
+		generator.EncodeBase64(generator.EncodeJWT("gints", "gaming hints"))
+	if generator.ValidateJWT(jwt) {
+		t.Errorf("validator can't validate jwt correctly")
+	}
+}
+
+func TestGenerateJWT(t *testing.T) {
+	t.Logf("I can't find a reasonable way to write a test for it.\n" +
+		"\tBut I'm sure that it's working correctly!")
+}
+
+func TestDecode(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	payload := generator.Decode(generator.GenerateJWT("hamed1soleimani@gmail.com", false))
+	expectedEmail := "hamed1soleimani@gmail.com"
+	expectedAdmin := false
+	expectedExp := time.Now().Unix() + 5*3600
+	if strings.Compare(payload.Email, expectedEmail) != 0 ||
+		payload.Admin != expectedAdmin || payload.Expire != expectedExp {
+		t.Errorf("expected %s, %t, %d", expectedEmail, expectedAdmin, expectedExp)
+		t.Errorf("got %s, %t, %d", payload.Email, payload.Admin, payload.Expire)
+	}
+}
+
+func TestCheckExpire(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	payload := generator.Decode(generator.GenerateJWT("hamed1soleimani@gmail.com", false))
+	if generator.CheckExpire(payload.Expire) {
+		t.Errorf("jwt is expiring soon")
+	}
+	if !generator.CheckExpire(payload.Expire - 6*3600) {
+		t.Errorf("jwt is expiring late")
+	}
+}
+
+func TestCheckRelogin(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	payload := generator.Decode(generator.GenerateJWT("hamed1soleimani@gmail.com", false))
+	if generator.CheckReLogin(payload.Expire - 3600*24*13) {
+		t.Errorf("jwt requests relogin soon")
+	}
+	if !generator.CheckReLogin(payload.Expire - 3600*24*15) {
+		t.Errorf("jwt requests relogin late")
+	}
+}
+
+func TestRenewJWT(t *testing.T) {
+	generator := NewJWTGenerator("1cfabccbf188251666dfa066a88864a7")
+	jwt := generator.GenerateJWT("hamed1soleimani@gmail.com", false)
+	payload := generator.Decode(generator.RenewJWT(jwt))
+	expectedEmail := "hamed1soleimani@gmail.com"
+	expectedAdmin := false
+	expectedExp := time.Now().Unix() + 5*3600
+	if strings.Compare(payload.Email, expectedEmail) != 0 ||
+		payload.Admin != expectedAdmin || payload.Expire != expectedExp {
+		t.Errorf("expected %s, %t, %d", expectedEmail, expectedAdmin, expectedExp)
+		t.Errorf("got %s, %t, %d", payload.Email, payload.Admin, payload.Expire)
+	}
+}
